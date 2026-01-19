87733ec24a78fb981a787a64e28565555735f970
Gonzalo Facello <facello.gonzalo@gmail.com>
2026-01-18 22:03:37 -0300

    fix: remove deprecated unit attribute from UteTariffSensor

--- custom_components/ute_tariff/sensor.py
+++ custom_components/ute_tariff/sensor.py
@@ -98,7 +98,6 @@ class UteTariffSensor(CoordinatorEntity[UteTariffCoordinator], SensorEntity):
         self.entity_description = description
         self._attr_name = description.name
         self._attr_unique_id = f"{entry.entry_id}_{description.key}"
-        self._attr_native_unit_of_measurement = description.unit
         self._attr_device_class = description.device_class
         self._attr_state_class = description.state_class
         self._entry = entry
